70d8bdc2ede0ab5ee9088157a6ff04000ad03904
mrivanova18 <m.r.ivanova2019@pmg-pernik.com>
2024-02-12 11:31:02 +0200

    added message

--- Dogs/Dogs/Program.cs
+++ Dogs/Dogs/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, Rocky!");
+            Console.WriteLine("Zovem samo da ti kajem OSTAVQM TE!!!");
         }
     }
 }
